0b4bbb585130ee91c1af7d45327deb437321ec95
Tsyrik <forgamesandother@gmail.com>
2024-03-12 23:22:31 -0600

    Add "next" block and sounds play

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ clock = pygame.time.Clock()
 game = Game()
 
 GAME_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(GAME_UPDATE, 200)
+pygame.time.set_timer(GAME_UPDATE, 300)
 
 while True:
     for event in pygame.event.get():
